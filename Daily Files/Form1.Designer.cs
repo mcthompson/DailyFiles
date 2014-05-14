fb5024d7dbaaef29a571715a29ccfa41d215957c
mcthompson <mcthompson@crimson.ua.edu>
2014-05-14 10:38:31 -0500

    Working on date range functionality

---
+++ Daily Files/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Daily_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tfButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.mondayButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.dateTo = new System.Windows.Forms.DateTimePicker();
+            this.dateFrom = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tfButton
+            // 
+            this.tfButton.Location = new System.Drawing.Point(48, 55);
+            this.tfButton.Name = "tfButton";
+            this.tfButton.Size = new System.Drawing.Size(80, 30);
+            this.tfButton.TabIndex = 0;
+            this.tfButton.Text = "Run";
+            this.tfButton.UseVisualStyleBackColor = true;
+            this.tfButton.Click += new System.EventHandler(this.tfButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.tfButton);
+            this.panel2.Location = new System.Drawing.Point(12, 9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(184, 114);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Day of Week: Tuesday - Friday";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.mondayButton);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(12, 139);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(184, 108);
+            this.panel3.TabIndex = 4;
+            // 
+            // mondayButton
+            // 
+            this.mondayButton.Location = new System.Drawing.Point(48, 49);
+            this.mondayButton.Name = "mondayButton";
+            this.mondayButton.Size = new System.Drawing.Size(80, 30);
+            this.mondayButton.TabIndex = 1;
+            this.mondayButton.Text = "Run";
+            this.mondayButton.UseVisualStyleBackColor = true;
+            this.mondayButton.Click += new System.EventHandler(this.mondayButton_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Day of Week: Monday";
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(215, 12);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(216, 235);
+            this.logBox.TabIndex = 5;
+            // 
+            // dateTo
+            // 
+            this.dateTo.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTo.Location = new System.Drawing.Point(234, 49);
+            this.dateTo.Name = "dateTo";
+            this.dateTo.Size = new System.Drawing.Size(99, 21);
+            this.dateTo.TabIndex = 6;
+            // 
+            // dateFrom
+            // 
+            this.dateFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateFrom.Location = new System.Drawing.Point(105, 49);
+            this.dateFrom.MaxDate = new System.DateTime(2014, 5, 13, 0, 0, 0, 0);
+            this.dateFrom.Name = "dateFrom";
+            this.dateFrom.Size = new System.Drawing.Size(99, 21);
+            this.dateFrom.TabIndex = 7;
+            this.dateFrom.Value = new System.DateTime(2014, 5, 13, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "To:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dateTo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dateFrom);
+            this.panel1.Location = new System.Drawing.Point(12, 262);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 113);
+            this.panel1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(353, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Or, select a date range for which you wish to generate EXD files.";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(163, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "From:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(445, 385);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EXD Daily Files";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tfButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button mondayButton;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.DateTimePicker dateTo;
+        private System.Windows.Forms.DateTimePicker dateFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Daily Files/Form1.cs
@@ -0,0 +1,246 @@
+﻿//Created by Morgan Thompson 
+//Updated: 5/13/2014 
+//Contact: morganchristopherthompson@gmail.com 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace Daily_Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dateTo.MaxDate = DateTime.Today;
+            dateFrom.MaxDate = DateTime.Today;
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            if (today == DayOfWeek.Monday)
+            {
+                tfButton.Enabled = false;
+            }
+            else
+            {
+                mondayButton.Enabled = false;
+            }
+        }
+
+        public void email()
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            logBox.AppendText(Environment.NewLine);
+            logBox.AppendText("Checking for EXD files...");
+            if (((File.Exists(exdFile(0)))) || ((File.Exists(exdFile(1)))) || ((File.Exists(exdFile(2)))))
+            // Checks to see if there are EXD files saved for today, 1 day prior, 2 days prior
+            {
+                Process.Start(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Office Outlook 2007.lnk");
+                Task.Delay(10000);
+                logBox.AppendText(Environment.NewLine);
+                logBox.AppendText("Sending email...");
+                try
+                {
+                    // Create the Outlook application.
+                    Outlook.Application oApp = new Outlook.Application();
+                    // Create a new mail item.
+                    Outlook.MailItem oMsg = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+                    // Set HTMLBody. 
+                    //add the body of the email
+                    oMsg.HTMLBody = "Please see attached.";
+                    //Add an attachment.
+                    String sDisplayName = "MyAttachment";
+                    int iPosition = (int)oMsg.Body.Length + 1;
+                    int iAttachType = (int)Outlook.OlAttachmentType.olByValue;
+                    //now attached the file
+                    if (today == DayOfWeek.Monday)
+                    {
+                        if ((File.Exists(exdFile(0)))) // Checks to see if there is an EXD file for Monday 
+                        { Outlook.Attachment aAttach = oMsg.Attachments.Add(exdFile(0), iAttachType, iPosition, sDisplayName); }
+
+                        if ((File.Exists(exdFile(1)))) // Checks to see if there is an EXD file for Sunday 
+                        { Outlook.Attachment bAttach = oMsg.Attachments.Add(exdFile(1), iAttachType, iPosition, sDisplayName); }
+
+                        if ((File.Exists(exdFile(2)))) // Checks to see if there is an EXD file for Saturday
+                        { Outlook.Attachment cAttach = oMsg.Attachments.Add(exdFile(2), iAttachType, iPosition, sDisplayName); }
+                    }
+                    else
+                    {
+                        if ((File.Exists(exdFile(0))))
+                        { Outlook.Attachment aAttach = oMsg.Attachments.Add(exdFile(0), iAttachType, iPosition, sDisplayName); }
+                    }
+
+                    //Subject line
+                    oMsg.Subject = "EXD Daily Files";
+                    // Add a recipient.
+                    Outlook.Recipients oRecips = (Outlook.Recipients)oMsg.Recipients;
+                    // Change the recipient in the next line if necessary.
+                    Outlook.Recipient oRecip = (Outlook.Recipient)oRecips.Add("morganchristopherthompson@gmail.com");
+                    oRecip.Resolve();
+                    // Send.
+                    oMsg.Send();
+                    // Clean up.
+                    oRecip = null;
+                    oRecips = null;
+                    oMsg = null;
+                    oApp = null;
+                }//end of try block
+                catch (Exception ex)
+                {
+                }//end of catch
+            }
+            else
+            {
+                logBox.AppendText(Environment.NewLine);
+                logBox.AppendText("No EXD files found! No email will be sent.");
+                return;
+            }
+        }
+ 
+        private string exdFile(int value)
+        {
+            string file = @"J:\Academic Outreach\Banner Files\EXD\EXD_";
+            int zero = 0;
+            int month = DateTime.Today.Month;
+            int day = DateTime.Today.Day - value;
+            int year = DateTime.Today.Year;
+            //File path includes a zero before month and day, but DateTime day/month does not contain zero. 
+            //This will add zero before month/day if needed. 
+            if (month <= 10) 
+            { 
+                file += zero.ToString();
+                file += month.ToString();
+            }
+            else { file += month.ToString(); }
+            if (day <= 10) 
+            { 
+                file += zero.ToString();
+                file += day.ToString();
+            }
+            else { file += day.ToString(); }
+            file += year.ToString();
+            file += ".xls";
+            return file;
+        }
+
+        private void openAccess()
+        {
+            System.Diagnostics.ProcessStartInfo p = new System.Diagnostics.ProcessStartInfo(@"J:\Academic Outreach\Banner Files\BannerData.mdb");
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            logBox.AppendText(Environment.NewLine);
+            logBox.AppendText("Opening the Access Database...");
+            proc.StartInfo = p;
+            proc.Start();
+            //Running the Access Database to generate EXD Excel spreadsheet
+            proc.WaitForExit();
+        }
+
+        private string filePath(int value)
+        {
+            string filePath = @"\\NEWDISTED2\ftproot\archive\archive_ao_regfile";
+            int year = DateTime.Today.Year;
+            int zero = 0;
+            int month = DateTime.Today.Month;
+            int day = DateTime.Today.Day - value;
+            string code = @"0946.txt";
+            string fileDate = year.ToString();
+            if (month <= 10) { fileDate += zero.ToString(); }
+            else { fileDate += month.ToString(); }
+            if (day <= 10) { fileDate += zero.ToString(); }
+            else { fileDate += day.ToString(); }
+            fileDate += code;
+            filePath += fileDate;
+            return filePath;
+        }
+
+        private void tfButton_Click(object sender, EventArgs e)
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            Console.WriteLine(today);
+            //Copying the ao_regfile to Banner Files
+            System.IO.File.Copy(@"\\NEWDISTED2\ftproot\ao_regfile.txt", @"J:\Academic Outreach\Banner Files\ao_regfile.txt", true);
+            logBox.AppendText(Environment.NewLine);
+            logBox.AppendText("Copying ao_regfile to Banner Files...");
+            openAccess();
+            email();
+            logBox.AppendText(Environment.NewLine);
+            logBox.AppendText("Done!");
+        }
+
+        private void mondayButton_Click_1(object sender, EventArgs e)
+        {
+            //Copying the ao_regfile from Archive to Banner Files
+            if (File.Exists(filePath(0)))
+            {
+                File.Copy(filePath(0), @"J:\Academic Outreach\Banner Files\ao_regfile.txt", true);  //This is Monday's file
+                logBox.AppendText(Environment.NewLine);
+                logBox.AppendText("Copying today's file...");
+                openAccess();
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Error! The file: " + filePath(0) + " could not be found!");
+                return;
+            }
+
+            if (File.Exists(filePath(1)))
+            {
+                File.Copy(filePath(1), @"J:\Academic Outreach\Banner Files\ao_regfile.txt", true);  //This is Sunday's file
+                logBox.AppendText(Environment.NewLine);
+                logBox.AppendText("Copying Sunday's file...");
+                openAccess();
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Error! The file: " + filePath(1) + " could not be found!");
+                return;
+            }
+
+            if (File.Exists(filePath(2)))
+            {
+                File.Copy(filePath(2), @"J:\Academic Outreach\Banner Files\ao_regfile.txt", true);  //This is Saturday's file
+                logBox.AppendText(Environment.NewLine);
+                logBox.AppendText("Copying Saturday's file...");
+                openAccess();
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Error! The file: " + filePath(2) + " could not be found!");
+                return;
+            }
+
+            //Now we've saved all of the EXD files... let's open that email!
+            email();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fromDate = dateFrom.Value.Day;
+            int toDate = dateTo.Value.Day;
+            int days = toDate - fromDate;
+            Console.WriteLine(days);
+           
+            if (dateTo.Value <= dateFrom.Value) { System.Windows.Forms.MessageBox.Show("Error: The 'To' date is before or equal to the 'From' date. Please fix this to continue."); }
+            else
+            {
+                for (int i = 0; i < days; i++)
+                {
+                    if (File.Exists(filePath(i))) 
+                    { 
+                    File.Copy(filePath(i), @"J:\Academic Outreach\Banner Files\ao_regfile.txt", true);
+                    openAccess();
+                    }
+                    }
+            }
+        }
+    }
+}
+
